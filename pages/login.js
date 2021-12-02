bbed885d92bb814f2f74e19df4fab83d8b3ddb57
Jagrath <jagrathshettylxix@gmail.com>
2021-12-02 12:00:31 +0530

    NextAuth Authentication implemented

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,20 @@
-function Login() {
+import { getProviders, signIn } from "next-auth/react";
+
+function Login({ providers }) {
   return (
     <div>
-      <h1>This is the login page</h1>
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
     </div>
   );
 }
 
 export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+}
