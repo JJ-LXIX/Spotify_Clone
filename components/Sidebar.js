0f0da281be2d7cd597bb5e07781a256243baab7b
Jagrath <jagrathshettylxix@gmail.com>
2021-12-06 16:27:25 +0530

    Song and Songs Components

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,7 +7,7 @@ import {
   HeartIcon,
   RssIcon,
 } from "@heroicons/react/outline";
-import { signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import useSpotify from "../hooks/useSpotify";
 import { useRecoilState } from "recoil";
 import { playlistIdState } from "../atoms/playlistAtom";
@@ -33,12 +33,6 @@ function Sidebar() {
   return (
     <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
       <div className="space-y-4">
-        <button
-          onClick={() => signOut()}
-          className="flex items-center space-x-2 hover:text-white"
-        >
-          <p>Logout</p>
-        </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
@@ -69,7 +63,7 @@ function Sidebar() {
         {playlists.map((playlist) => (
           <p
             key={playlist.id}
-            className="playlist-cursor-pointer hover:text-white"
+            className="playlist-cursor-pointer cursor-pointer hover:text-white"
             onClick={() => setPlaylistsId(playlist.id)}
           >
             {playlist.name}
